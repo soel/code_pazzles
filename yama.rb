7fd5257da3cbab50f937a8a75714f970905d2f02
kanbara <kanbara@bbtower.co.jp>
2015-06-16 11:17:04 +0900

    経路コスト計算

--- yama.rb
+++ yama.rb
@@ -1,6 +1,6 @@
 #! /home/kanbara/.rbenv/shims/ruby
 
-maparr = [5,5,4,2,1,1,1,2,2,3,
+@maparr = [5,5,4,2,1,1,1,2,2,3,
 		      5,4,3,2,1,1,2,2,3,4,
 		      5,4,2,1,1,2,2,4,5,5,
 		      4,4,2,1,2,2,3,3,4,5,
@@ -13,14 +13,54 @@ maparr = [5,5,4,2,1,1,1,2,2,3,
 
 #p maparr[0 + 1 * 10]
 
-w = 10
-h = 10
+@h = 10
+@w = 10
+@x1 = 4
+@y1 = 0
+@x2 = 5
+@y2 = 9
 
-a = []
+@arr = []
 i = 0
-while i < w * h do
-	a[i] = "inf"
+while i < @w * @h do
+	@arr[i] = 10000 #予想される計算結果より大きい値にしておく
 	i += 1
 end
 
-p a
+def gencstmap(x, y, cst)
+	cst += @maparr[x + y * @w]
+	if cst >= @arr[x + y * @w]
+		return
+	end
+	@arr[x + y * @h] = cst
+
+	if x > 0
+		gencstmap(x-1 , y, cst)
+	end
+	if y > 0
+		gencstmap(x, y-1, cst)
+	end
+	if x < @w - 1
+		gencstmap(x+1, y , cst)
+	end
+	if y < @h - 1
+		gencstmap(x, y+1, cst)
+	end
+end
+
+gencstmap(@x2, @y2, - @maparr[@x2 + @y2 * @h])
+
+y = 0
+while y < @h do
+	s = ""
+	x = 0
+	while x < @w do
+		c = @arr[x + y * @w]
+		s += c < 10 ? " " + c.to_s : c.to_s
+		s += ","
+		x += 1
+	end
+
+	p s
+	y += 1
+end
